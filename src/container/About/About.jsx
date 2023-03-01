3ba47770d60634eb4e13e123a8d4aefa0923f2da
Kamary <gillespiekamary@gmail.com>
2023-03-01 12:35:43 -0700

    testing deployment

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import React from "react";
+//import { motion } from "framer-motion";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
 import "./About.scss";
-import { client, urlFor } from "../../client";
+//import { client } from "../../client";
 
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
-
-  useEffect(() => {
-    client
-      .fetch(
-        `*[_type == "abouts"]{
-        title,
-        description,
-        mainImage{
-          asset->{
-            _id,
-            url
-          },
-          alt
-        }
-      }`
-      )
-      .then((data) => {
-        setAbouts(data);
-      })
-      .catch(console.error);
-  }, []);
-
   return (
     <>
       <p className="head-text">
@@ -44,25 +21,7 @@ const About = () => {
         watching movies with my friends and family.
       </p>
 
-      <div className="app__profiles">
-        {/* {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.mainImage)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
-        ))} */}
-      </div>
+      <div className="app__profiles"></div>
     </>
   );
 };
